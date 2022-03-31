7e97be5334fb4da64c63a4cc8b25de8bd787bd80
sittisafiatun <85608773+sittisafiatun@users.noreply.github.com>
2022-03-31 12:59:09 +0700

    Add files via upload

---
+++ Koneksi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//1
+using System.Data.SqlClient;
+
+namespace KasirBaru_Pertemuan7
+{
+	public class Koneksi
+	{
+		public SqlConnection GetConn(){
+			SqlConnection Conn = new SqlConnection();
+			Conn.ConnectionString = "Data Source=DESKTOP-15TBTTA; initial catalog=Kasir; integrated security=true";
+			return Conn;
+       	} 
+	}
+}
